7167311ab42a4d4e7517ad5921f941652c1c5d2c
Vasileios Klouvatos <bilklo@gmail.com>
2015-02-22 19:47:15 +0200

    Two functions. The first one manage the gettters and setters of the matrix the second one manage the inverse table

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##First functions create a matrix and manager getters, setters and the inverse table
+##Second function returns the inverse of a matrix. If the matrix is cached then
+##it returns the cached value
 
-## Write a short comment describing this function
-
+## Creates a matrix and manage getters, setters of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <-NULL
+  set <- function(y){
+    x<<-y
+    m<<-NULL
+  }
+  
+  get <-function() x
+  setInversTable <- function(inversTable) m<<-inversTable
+  getInversTable <- function() m
+  list(set=set, get=get,
+       setInversTable = setInversTable,
+       getInversTable = getInversTable)
 }
 
 
 ## Write a short comment describing this function
+##This function returns the inverse of matrix
+##if the value is cached then it returns the 
+##the value from memory (cached)
 
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getInversTable()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <-x$get()
+  m <- solve(data,...)
+  x$setInversTable(m)
+  m
 }
